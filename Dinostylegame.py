fed9c0bdaa53550d95bdec6b318b5659837759c5
Swayam Shah <helloswayamshah@gmail.com>
2021-10-13 08:50:36 +0530

    Leaderboard still not fixed

--- Dinostylegame.py
+++ Dinostylegame.py
@@ -131,7 +131,7 @@ class Cloud:
 
 
 def main():
-    global game_speed, x_pos_bg, y_pos_bg, points, obstacles, highscore, run
+    global game_speed, x_pos_bg, y_pos_bg, points, obstacles, highscore, run, l
     run = True
     player = Dinosaur()
     clock = pygame.time.Clock()
@@ -140,27 +140,30 @@ def main():
     x_pos_bg = 0
     y_pos_bg = 380
     points = 0
-    highscore = []
+    highscore = [0]
+    l = len(highscore)
     font = pygame.font.Font('freesansbold.ttf', 20)
     obstacles = []
 
     def Score():
-        global points, game_speed, highscore, run
+        global points, game_speed, highscore, run, l
         points += 1
 
         if points % 100 == 0:
             game_speed += 1
 
             if run is False:
-                if points > highscore:
-                    highscore = points
+                l = len(highscore)
+                if points > highscore[l-1]:
+                    highscore.append(points)
 
         text = font.render("Score: " + str(points), True, (0, 0, 0))
         textrect = text.get_rect()
         textrect.center = (1000, 40)
         Screen.blit(text, textrect)
 
-        text1 = font.render("HI: " + str(highscore), True, (0, 0, 0))
+
+        text1 = font.render("HI: " + str(highscore[l-1]), True, (0, 0, 0))
         text1rect = text1.get_rect()
         text1rect.center = (875, 40)
         Screen.blit(text1, text1rect)
@@ -216,7 +219,7 @@ def score():
     run = True
     while run:
         global highscore
-        highscore = []
+        highscore = [0]
 
         Screen.fill((0, 0, 0))
 
@@ -227,12 +230,9 @@ def score():
 
         Screen.blit(text, textrect)
 
-        file = open ('score.txt','w+')
+        file = open('score.txt', 'w+')
 
         file.writelines(highscore)
-
-
-
 
         for x in range(1, 5):
             read = file.readline()
@@ -242,9 +242,11 @@ def score():
             textrect = text.get_rect()
             textrect.center = (550, 100)
             Screen.blit(text, textrect)
-            pygame.draw.rect(Screen, (255, 255, 255), (400, 375, 300, 50), 3, 30)
+            pygame.draw.rect(Screen, (255, 255, 255),
+                             (400, 375, 300, 50), 3, 30)
 
         file.close()
+
 
 def menu():
     run = True
